b9621f75ab64952a72c539c4e0be3838421ca060
me990928 <me990928@gmail.com>
2023-12-12 12:46:18 +0900

    スキーマの追加

--- src/schema/schedule_schema.py
+++ src/schema/schedule_schema.py
@@ -8,3 +8,12 @@ class MovieScheduleSchema(BaseModel):
     f_movie_start_datetime: datetime
     f_movie_end_datetime: datetime
     model_config = ConfigDict(from_attributes=True)
+
+class TheaterScheduleSchema(BaseModel):
+    f_theater_schedule_id: int | None = None
+    f_screen_id: str
+    f_movie_schedule_id: str
+    f_movie_start_datetime: datetime
+    f_release_end_date: datetime
+
+    model_config = ConfigDict(from_attributes=True)
