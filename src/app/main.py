3622d5f7928d64fcd33884d55ca7fcb534d9af86
me990928 <me990928@gmail.com>
2023-12-12 10:04:57 +0900

    エラーハンドリングの追加

--- src/app/main.py
+++ src/app/main.py
@@ -14,5 +14,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.requests import Request
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": exc.errors(), "body": exc.body},
+    )
 
 app.include_router(api_router, prefix="/api/v1")
