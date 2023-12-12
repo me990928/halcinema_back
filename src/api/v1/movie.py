9a69c4615805699b36a5326118c59b5b30db1b02
me990928 <me990928@gmail.com>
2023-12-12 10:04:31 +0900

    動画情報のAPIの追加

--- src/api/v1/movie.py
+++ src/api/v1/movie.py
@@ -1,8 +1,12 @@
 import os
 from fastapi import APIRouter, File, UploadFile, Form
 from fastapi.params import Depends
-from typing import List, Optional
+from typing import List, Optional, Any
 import sys
+from PIL import Image
+import logging
+import base64
+import io
 
 from sqlalchemy import select
 sys.path.append("../../")
@@ -17,6 +21,11 @@ from schema.movie_schema import MovieTitleSchema
 from schema.movie_schema import MoviePictSchema
 from itertools import chain
 from model.m_movie_pict import MoviePict
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from model.m_movie_genres import MovieGenre
+from schema.movie_schema import MovieGenreSchema
 
 router = APIRouter()
 
@@ -26,8 +35,18 @@ def read_root():
 
 @router.get("/get")
 def get_movie1(db: Session = Depends(get_db)):
-    q = db.query(Movie, MovieTitle).join(Movie, Movie.f_movie_title_id == MovieTitle.f_movie_title_id).limit(100).all()  
-    return [{k: v for k, v in chain(movie.__dict__.items(), title.__dict__.items()) if not k.startswith('_')} for movie, title in q]
+    # q = db.query(Movie, MovieTitle).join(Movie, Movie.f_movie_title_id == MovieTitle.f_movie_title_id).limit(100).all()  
+    # クエリを作成
+    q = db.query(
+        Movie,
+        MovieTitle,
+        MovieGenre
+    ).join(
+        MovieGenre, Movie.f_genre_id == MovieGenre.f_genre_id
+    ).join(
+        MovieTitle, Movie.f_movie_title_id == MovieTitle.f_movie_title_id
+    ).limit(100)
+    return [{k: v for k, v in chain(movie.__dict__.items(), title.__dict__.items(), genre.__dict__.items()) if not k.startswith('_')} for movie, title, genre in q]
  
 @router.post("/create")
 def add_movie(movie_schema: MovieSchema, db: Session = Depends(get_db)):
@@ -67,29 +86,107 @@ def increment_f_movie_id(f_movie_id):
 def create_movie_title(movie_title_schema: MovieTitleSchema, db: Session = Depends(get_db)):
     return add_movie_title(movie_title_schema, db)
 
+# @router.post("/upload_picture")
+# async def upload_picture(f_movie_pict_id: Optional[int] = Form(None), f_movie_id: str = Form(...), f_movie_pict: str = Form(...), files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
+
+   
+#     # filesの要素数をログ出力
+#     logging.info(f"Number of files: {len(files)}")
+
+
+#     try:
+#         for file in files:
+#             if file.content_type != "image/jpeg":
+#                 return {"msg": "画像の拡張子はjpegにしてください"}
+#     except:
+#         return {"msg": "画像の拡張子はjpegにしてください"}
+
+#     try:
+#         for file in files:
+#             file_path = f"../../images/{f_movie_id}/{f_movie_pict}.jpeg"
+#             os.makedirs(os.path.dirname(file_path), exist_ok=True)
+#             with open(file_path, "wb") as buffer:
+#                 buffer.write(file.file.read())
+#     except:
+#         return {"msg": "画像の保存に失敗しました"}
+
+#     movie_pict_schema = MoviePictSchema(f_movie_id=f_movie_id, f_movie_pict=f_movie_pict)
+#     movie_pict = MoviePict(**movie_pict_schema.dict(exclude={"f_movie_pict_id"}))
+#     db.add(movie_pict)
+#     db.commit()
+#     db.refresh(movie_pict)
+
+#     return {"msg": "success"}
+
 @router.post("/upload_picture")
-async def upload_picture(f_movie_pict_id: Optional[int] = Form(None), f_movie_id: str = Form(...), f_movie_pict: str = Form(...), files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
-    # 画像の拡張子がjpegかの確認
-    try:
-        for file in files:
-            if file.content_type != "image/jpeg":
-                return {"msg": "画像の拡張子はjpegにしてください"}
-    except:
-        return {"msg": "画像の拡張子はjpegにしてください"}
+async def upload_files(movieId: str = Form(...), image: UploadFile = File(...), name: str = Form(...)):
+    # movieIdとfile_data（UploadFile）がリクエストで受け取られる
+    # contents = await file_data.read()
+    # contentsにはファイルのバイナリデータが含まれます
+
+    # ここでcontentsを処理する（例：保存する、変換する、または画像として扱うなど）
+
+    print(name)
+
+    # 画像をバイナリデータに変換
+    contents = await image.read()
+
+    # print(contents.decode("utf-8"))
+
+    # print(type(contents))
+
+    # bytesをstrに変換
+    contents_str = contents.decode("utf-8")
+    base64_str = contents_str.split(",")[1]
+
+    # print(base64_str)
+
+    # フォルダが無kレバーない場合は作成
+    os.makedirs(f"../../images/{movieId}", exist_ok=True)
+
+    # strを画像に変換
+    img = Image.open(io.BytesIO(base64.b64decode(base64_str)))
+    img.save(f"../../images/{movieId}/{name}.jpeg", "JPEG")
+    
+    return {"movieId": movieId}
+
+@router.get("/genre")
+def get_genre(db: Session = Depends(get_db)):
+    q = db.query(MovieGenre).distinct().all()
+    return q
+
+@router.post("/genre")
+def add_genre(genre_shema: MovieGenreSchema , db: Session = Depends(get_db)):
+
+    
+
+    query = select(MovieGenre.f_genre_id).order_by(MovieGenre.f_genre_id.desc()).limit(1)
+    result = db.execute(query)
+
+    # クエリ結果からf_movie_idを取得
+    f_genre_id = result.scalar()
 
     try:
-        for file in files:
-            file_path = f"../../images/{f_movie_id}/{f_movie_pict}.jpeg"
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            with open(file_path, "wb") as buffer:
-                buffer.write(file.file.read())
+        new_f_genre_id = increment_f_genre_id(f_genre_id)
     except:
-        return {"msg": "画像の保存に失敗しました"}
+        new_f_genre_id = "G0000001"
 
-    movie_pict_schema = MoviePictSchema(f_movie_id=f_movie_id, f_movie_pict=f_movie_pict)
-    movie_pict = MoviePict(**movie_pict_schema.dict(exclude={"f_movie_pict_id"}))
-    db.add(movie_pict)
+    genre_shema.f_genre_id = new_f_genre_id
+
+    genre = MovieGenre(**genre_shema.dict())
+    db.add(genre)
     db.commit()
-    db.refresh(movie_pict)
-
+    db.refresh(genre)
     return {"msg": "success"}
+
+def increment_f_genre_id(f_genre_id):
+    # f_movie_idの現在の値を取得
+    current_value = int(f_genre_id[2:])
+
+    # f_movie_idをインクリメント
+    incremented_value = current_value + 1
+
+    # インクリメントされたf_movie_idをMG000001のような形式に変換
+    new_f_movie_id = "G" + str(incremented_value).zfill(7)
+
+    return new_f_movie_id
