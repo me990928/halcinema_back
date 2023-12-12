3a5174c0f779948194b8e3ea196274a56e88ebdc
me990928 <me990928@gmail.com>
2023-12-12 12:16:38 +0900

    スクリーンIDと番号の取得 #15

--- src/api/v1/schedule.py
+++ src/api/v1/schedule.py
@@ -7,10 +7,11 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from model.t_theater_schedules import TheaterSchedule, MovieSchedule
 from model.m_movies import Movie
+from model.m_screens import Screen
 from schema.schedule_schema import MovieScheduleSchema
 from sqlalchemy import select
 from itertools import chain
-
+import re
 
 
 
@@ -31,3 +32,15 @@ def add_movie_schedule(movie_schedule_schema: MovieScheduleSchema, db: Session =
 def get_movie_schedule(db: Session = Depends(get_db)):
     q = db.query(MovieSchedule.f_movie_schedule_id, MovieSchedule.f_movie_schedule_name, Movie.f_movie_runtime_min).join(Movie, Movie.f_movie_id == MovieSchedule.f_movie_id).limit(100)
     return [{"movie_schedule_id": movie_schedule_id, "movie_schedule_name": movie_schedule_name, "movie_runtime_min": movie_runtime_min} for movie_schedule_id, movie_schedule_name, movie_runtime_min in q]
+
+@router.get("/screen")
+def get_screen(db: Session = Depends(get_db)):
+    q = db.query(Screen).limit(100)
+
+    # return [
+    #     {k: v for k, v in screen.__dict__.items() if not k.startswith('_')} for screen in q]
+    return [{**{k: v for k, v in screen.__dict__.items() if not k.startswith('_')}, "screen_name": get_screen_name(screen.f_screen_id)} for screen in q]
+
+def get_screen_name(f_screen_id):
+    match = re.search(r'SCR000([A-Z]\d)', f_screen_id)
+    return match.group(1)[0] + match.group(1)[1]
